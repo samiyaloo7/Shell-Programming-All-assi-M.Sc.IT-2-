60522bca886e2cd61f9ef35171da864bddc6420c
$amiyal : ) <samiyaloo7@samiyalization.github.com>
2020-08-23 17:11:22 +0530

    matrix multiplication added

---
+++ Exam/matrix_multiplication.sh
@@ -0,0 +1,51 @@
+declare -A aar bar
+# Program to multiply two 3*3 matrix.
+echo "...Matrix Multiplication..."
+# Entering value to matrix 1.
+i=0
+while [ $i -lt 3 ]
+do
+    j=0
+    while [ $j -lt 3 ]
+    do
+        read -p "aar[$i,$j]" aar[$i,$j]
+        j=`expr $j + 1`
+    done
+    echo -e
+    i=`expr $i + 1`
+done
+# Entering value to matrix 2.
+i=0
+while [ $i -lt 3 ]
+do
+    j=0
+    while [ $j -lt 3 ]
+    do
+        read -p "bar[$i,$j]" bar[$i,$j]
+        j=`expr $j + 1`
+    done
+    echo -e
+    i=`expr $i + 1`
+done
+# Displaying matrix multiplication.
+i=0
+while [ $i -lt 3 ]
+do
+    j=0
+    while [ $j -lt 3 ]
+    do
+        k=0
+        temp=0
+        mul=0
+        while [ $k -lt 3 ]
+        do
+            temp=`expr ${aar[$i,$k]} \* ${bar[$k,$j]}`
+            mul=`expr $mul + $temp`
+            k=`expr $k + 1`
+        done
+        echo -n "= "$mul" "
+        j=`expr $j + 1`
+    done
+    echo -e
+    i=`expr $i + 1`
+done
